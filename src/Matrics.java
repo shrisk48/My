d74c396872b6bf32c6bae3da7c8b7f65e4546a8d
unknown <shrinivas.kottawar04@gmail.com>
2021-04-29 22:28:07 +0530

    Updated Program:

--- src/Matrics.java
+++ src/Matrics.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Matrics {
+public class Matrics {
+    public static void main(String[] args)
+    {
+        int [][] arr1= {{5,8},
+                       {9,3}};
+        int [][] arr2= {{5,4},
+                {9,2}};
+        int [][] arr3=new int[2][2];
+        for(int i=0;i< arr1.length ;i++) {
+            for(int j=0;j< arr2.length;j++) {
+                arr3[i][j]=arr1[i][j]+arr2[i][j];
+            }
+        }
+        for(int i=0;i<arr3.length;i++) {
+                for(int j=0;j<arr3.length;j++) {
+                    System.out.print(arr3[i][j]+" ");
+                }
+                System.out.println() ;
+        }
+    }
 }
